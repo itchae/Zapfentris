1413de0e62d45de21057bf50ee931dfa583e0a57
GIacial <cristalsaga@hotmail.fr>
2016-03-16 18:39:56 +0100

    dimension fenetre

--- gestionFenetre.c
+++ gestionFenetre.c
@@ -17,7 +17,7 @@ void mainInterface(){
     systemJeu* jeu =NULL;
 
     SDL_Window* fenetre;                                                                                                //pointeur sur la fenetre
-    fenetre = SDL_CreateWindow("Zapfentris",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,SDL_WINDOW_SHOWN);  //cree la fenetre
+    fenetre = SDL_CreateWindow("Zapfentris",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1000,600,SDL_WINDOW_SHOWN);  //cree la fenetre
     if(fenetre== 0)                                                                                                     //verification que la fenetre a ete cree
     {
         printf("PROBLEME!! Erreur lors de la creation de la fenetre  \n" );                                             //gestion de l'erreur
