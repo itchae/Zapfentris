fed7e80713ef936a3f704780d63d94322b459c2e
GIacial <cristalsaga@hotmail.fr>
2016-04-25 21:26:00 +0200

    kimage

--- fenetreSauvegarde.c
+++ fenetreSauvegarde.c
@@ -20,10 +20,10 @@ void func_fenetreSauvegarde(SDL_Window* fenetre,SDL_Surface* ecran,systemJeu* je
     }
     SDL_FillRect(boutonSauv1,NULL,SDL_MapRGB(boutonSauv1->format,100,100,255));
 
-//    SDL_Surface* boutonSauv1Dessus = SDL_LoadBMP("Images/boutonSauv1.bmp");
-//    if(boutonSauv1Dessus==NULL){
-//        printf("PROBLEME!! erreur lors de la creation du bouton sauv1 de dessus\n");
-//    }
+      SDL_Surface* boutonSauv1Dessus = SDL_LoadBMP("Images/boutonSauv1.bmp");
+      if(boutonSauv1Dessus==NULL){
+          printf("PROBLEME!! erreur lors de la creation du bouton sauv1 de dessus\n");
+      }
 
     // SAUVEGARDE 2
     SDL_Surface* boutonSauv2 = SDL_CreateRGBSurface(0,300,70,32,0,0,0,0);
@@ -32,10 +32,10 @@ void func_fenetreSauvegarde(SDL_Window* fenetre,SDL_Surface* ecran,systemJeu* je
     }
     SDL_FillRect(boutonSauv2,NULL,SDL_MapRGB(boutonSauv2->format,100,100,255));
 
-//    SDL_Surface* boutonSauv2Dessus = SDL_LoadBMP("Images/boutonSauv2.bmp");
-//    if(boutonSauv2Dessus==NULL){
-//        printf("PROBLEME!! erreur lors de la creation du bouton sauv2 de dessus\n");
-//    }
+      SDL_Surface* boutonSauv2Dessus = SDL_LoadBMP("Images/boutonSauv2.bmp");
+      if(boutonSauv2Dessus==NULL){
+          printf("PROBLEME!! erreur lors de la creation du bouton sauv2 de dessus\n");
+      }
 
     //SAUVEGARDE 3
     SDL_Surface* boutonSauv3 = SDL_CreateRGBSurface(0,300,70,32,0,0,0,0);
@@ -44,33 +44,31 @@ void func_fenetreSauvegarde(SDL_Window* fenetre,SDL_Surface* ecran,systemJeu* je
     }
     SDL_FillRect(boutonSauv3,NULL,SDL_MapRGB(boutonSauv3->format,100,100,255));
 
-//    SDL_Surface* boutonSauv3Dessus = SDL_LoadBMP("Images/boutonSauv3.bmp");
-//    if(boutonSauv3Dessus==NULL){
-//        printf("PROBLEME!! erreur lors de la creation du bouton sauv3 de dessus\n");
-//    }
+      SDL_Surface* boutonSauv3Dessus = SDL_LoadBMP("Images/boutonSauv3.bmp");
+      if(boutonSauv3Dessus==NULL){
+          printf("PROBLEME!! erreur lors de la creation du bouton sauv3 de dessus\n");
+      }
 
     //CHARGER
-    SDL_Surface* boutonCharger = SDL_CreateRGBSurface(0,170,70,32,0,0,0,0);
+    SDL_Surface* boutonCharger = SDL_LoadBMP("Images/boutonCharger.bmp");
     if(boutonCharger==NULL){
         printf("PROBLEME !! erreur lors de la creation du bouton charger\n");
     }
-    SDL_FillRect(boutonCharger,NULL,SDL_MapRGB(boutonCharger->format,100,100,255));
 
 
 
     //NOUVELLE
-    SDL_Surface* boutonNouvelle = SDL_CreateRGBSurface(0,170,70,32,0,0,0,0);
+    SDL_Surface* boutonNouvelle = SDL_LoadBMP("Images/boutonnouveau.bmp");
     if(boutonNouvelle==NULL){
         printf("PROBLEME !! erreur lors de la creation du bouton nouvelle\n");
     }
-    SDL_FillRect(boutonNouvelle,NULL,SDL_MapRGB(boutonNouvelle->format,100,100,255));
 
 
 
     SDL_Event event;
 
     while(*typeFenetre==fenetreSauvegarde){
-        refreshFenetreSauvegarde (ecran, Sauvegarde, jeu, titre, boutonSauv1, /*boutonSauv1Dessus , */boutonSauv2, /*boutonSauv2Desuus,*/ boutonSauv3, /*boutonSauv3Dessus,*/ boutonCharger,
+        refreshFenetreSauvegarde (ecran, Sauvegarde, jeu, titre, boutonSauv1, boutonSauv1Dessus , boutonSauv2, boutonSauv2Dessus, boutonSauv3, boutonSauv3Dessus, boutonCharger,
                                 boutonNouvelle);
         SDL_UpdateWindowSurface(fenetre);
         SDL_WaitEvent(&event);
@@ -127,8 +125,8 @@ void func_fenetreSauvegarde(SDL_Window* fenetre,SDL_Surface* ecran,systemJeu* je
 
 }
 
-void refreshFenetreSauvegarde (SDL_Surface* ecran, E_Sauvegarde Sauvegarde,systemJeu* jeu,SDL_Surface* titre,SDL_Surface* boutonSauv1, /*SDL_Surface* boutonSauv1Dessus,*/
-                           SDL_Surface* boutonSauv2, /*SDL_Surface* boutonSauv2Desuus,*/ SDL_Surface* boutonSauv3, /*SDL_Surface* boutonSauv3Dessus,*/ SDL_Surface* boutonCharger,
+void refreshFenetreSauvegarde (SDL_Surface* ecran, E_Sauvegarde Sauvegarde,systemJeu* jeu,SDL_Surface* titre,SDL_Surface* boutonSauv1, SDL_Surface* boutonSauv1Dessus,
+                           SDL_Surface* boutonSauv2, SDL_Surface* boutonSauv2Dessus, SDL_Surface* boutonSauv3, SDL_Surface* boutonSauv3Dessus, SDL_Surface* boutonCharger,
                            SDL_Surface* boutonNouvelle)
 {
 
@@ -149,17 +147,17 @@ void refreshFenetreSauvegarde (SDL_Surface* ecran, E_Sauvegarde Sauvegarde,syste
     position.x=350;
     position.y=170;
     SDL_BlitSurface(boutonSauv1,NULL,ecran,&position);
-    //SDL_BlitSurface(boutonSauv1Dessus,NULL,ecran,&position);
+    SDL_BlitSurface(boutonSauv1Dessus,NULL,ecran,&position);
 
     position.x=350;
     position.y=270;
     SDL_BlitSurface(boutonSauv2,NULL,ecran,&position);
-    //SDL_BlitSurface(boutonSauv2Dessus,NULL,ecran,&position);
+    SDL_BlitSurface(boutonSauv2Dessus,NULL,ecran,&position);
 
     position.x=350;
     position.y=370;
     SDL_BlitSurface(boutonSauv3,NULL,ecran,&position);
-    //SDL_BlitSurface(boutonSauv3,NULL,ecran,&position);
+    SDL_BlitSurface(boutonSauv3Dessus,NULL,ecran,&position);
 
 
     if(fichier != NULL){
--- fenetreSauvegarde.h
+++ fenetreSauvegarde.h
@@ -8,8 +8,8 @@
 
 void func_fenetreSauvegarde(SDL_Window* fenetre,SDL_Surface* ecran,systemJeu* jeu,E_fenetre* typeFenetre);
 
-void refreshFenetreSauvegarde (SDL_Surface* ecran, E_Sauvegarde Sauvegarde,systemJeu* jeu,SDL_Surface* titre,SDL_Surface* boutonSauv1, /*SDL_Surface* boutonSauv1Dessus,*/
-                           SDL_Surface* boutonSauv2, /*SDL_Surface* boutonSauv2Desuus,*/ SDL_Surface* boutonSauv3, /*SDL_Surface* boutonSauv3Dessus,*/ SDL_Surface* boutonCharger,
+void refreshFenetreSauvegarde (SDL_Surface* ecran, E_Sauvegarde Sauvegarde,systemJeu* jeu,SDL_Surface* titre,SDL_Surface* boutonSauv1, SDL_Surface* boutonSauv1Dessus,
+                           SDL_Surface* boutonSauv2, SDL_Surface* boutonSauv2Dessus, SDL_Surface* boutonSauv3, SDL_Surface* boutonSauv3Dessus, SDL_Surface* boutonCharger,
                            SDL_Surface* boutonNouvelle);
 
 #endif // FENETRESAUVEGARDE_H_INCLUDED
